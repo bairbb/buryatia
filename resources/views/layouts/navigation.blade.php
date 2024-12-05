d5bdfb496b7d5f2d3e078744e8b6245cbfc5f07b
bairbb <qiukbb@gmail.com>
2024-12-05 19:53:09 +0800

    style search

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,7 +1,7 @@
 <nav x-data="{ open: false }" class="bg-white border-b border-gray-100">
     <!-- Primary Navigation Menu -->
     <div class="container mx-auto px-4 sm:px-6 lg:px-8">
-        <div class="flex justify-between  items-center h-16">
+        <div class="flex justify-between items-center h-16">
             <div class="flex">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
@@ -19,14 +19,14 @@
                         {{ __('Все места') }}
                     </x-nav-link>
                 </div>
-
             </div>
-
-            <form action="{{ route('search') }}" method="GET">
-                {{--                <x-input-label for="search" :value="__('Введите')"/>--}}
-
-                <x-text-input id="query" type="text" name="query" placeholder="Введите для поиска"/>
-                <x-secondary-button type="submit" class="">Найти</x-secondary-button>
+            <form action="{{ route('search') }}" method="GET" class="flex items-center gap-2">
+                <input type="text" name="query" placeholder="Поиск" class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm">
+                <button type="submit">
+                    <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="20" height="20" viewBox="0 0 30 30">
+                        <path d="M 13 3 C 7.4889971 3 3 7.4889971 3 13 C 3 18.511003 7.4889971 23 13 23 C 15.396508 23 17.597385 22.148986 19.322266 20.736328 L 25.292969 26.707031 A 1.0001 1.0001 0 1 0 26.707031 25.292969 L 20.736328 19.322266 C 22.148986 17.597385 23 15.396508 23 13 C 23 7.4889971 18.511003 3 13 3 z M 13 5 C 17.430123 5 21 8.5698774 21 13 C 21 17.430123 17.430123 21 13 21 C 8.5698774 21 5 17.430123 5 13 C 5 8.5698774 8.5698774 5 13 5 z"></path>
+                    </svg>
+                </button>
             </form>
 
             <!-- Settings Dropdown -->
