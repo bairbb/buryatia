fc63631ee231aff5462837fa2c5d25eb1a8416c2
bairbb <qiukbb@gmail.com>
2025-01-02 22:52:07 +0800

    add google analytics

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,6 +14,16 @@
 
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-MZXKTBSSNT"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-MZXKTBSSNT');
+    </script>
 </head>
 
 <body class="font-sans antialiased">
