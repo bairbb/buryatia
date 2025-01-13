dcca3dde351fed4420c6f5fc305d27d7d85f8002
bairbb <qiukbb@gmail.com>
2025-01-13 15:22:08 +0800

    edit logo

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -6,7 +6,7 @@
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('home') }}">
-                        <x-application-logo class="block h-7 w-auto fill-current text-white"/>
+                        <x-application-logo class="h-7 text-white fill-current"/>
                     </a>
                 </div>
 
